e728fac1c22dd3c275ba921f594d99865c43f11e
Weiss Jiang <meweissjiang@gmail.com>
2024-04-14 19:27:18 +1000

    init project and added first problem

---
+++ leetcode-solutions-csharp/Array&HashTable/Easy/ContainsDuplicate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_solutions_csharp.Array_HashTable.Easy
+{
+    /**
+     * 217. Contains Duplicate
+     */
+
+    public class ContainsDuplicate        
+    {
+        public void Run()
+        {
+            Console.WriteLine(Solution(new int[] { 1, 2, 3, 1 })); // true
+            Console.WriteLine(Solution(new int[] { 1, 2, 3, 4 })); // false
+            Console.WriteLine(Solution(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 })); // true
+        }
+
+        public bool Solution(int[] nums)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+
+            foreach (int item in hashSet)
+            {
+                if (hashSet.Contains(item))
+                {
+                    return true;
+                }
+
+                hashSet.Add(item);
+            }
+
+            return false;
+        }
+}
---
+++ leetcode-solutions-csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using leetcode_solutions_csharp.Array_HashTable.Easy;
+
+namespace leetcode_solutions_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+            containsDuplicate.Run();
+            Console.WriteLine("Press any key to exit...");
+        }
+    }
+}
