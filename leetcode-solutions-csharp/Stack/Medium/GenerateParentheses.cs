eee89c049444a868080b32a13a7157d910d3ca65
Weiss Jiang <meweissjiang@gmail.com>
2025-03-22 12:20:08 +1100

    added 22 Generate Parentheses

---
+++ leetcode-solutions-csharp/Stack/Medium/GenerateParentheses.cs
@@ -0,0 +1,48 @@
+﻿using leetcode_solutions_csharp.Utils.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_solutions_csharp.Stack.Medium;
+
+public class GenerateParentheses
+{
+    /*
+     * 22. Generate Parentheses
+     * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+     */
+
+    public void Run()
+    {
+        Console.WriteLine(ToStringHelper.NestedListToString(Solution(3))); // ["((()))","(()())","(())()","()(())","()()()"]
+        Console.WriteLine(ToStringHelper.NestedListToString(Solution(1))); // ["()"]
+    }
+
+    public IList<string> Solution(int n)
+    {
+        var result = new List<string>();
+        GenerateCombinations(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void GenerateCombinations(IList<string> result, string current, int open, int close, int max)
+    {
+        if (current.Length == max * 2)
+        {
+            result.Add(current);
+            return;
+        }
+
+        if (open < max)
+        {
+            GenerateCombinations(result, current + "(", open + 1, close, max);
+        }
+
+        if (close < open)
+        {
+            GenerateCombinations(result, current + ")", open, close + 1, max);
+        }
+    }
+}
