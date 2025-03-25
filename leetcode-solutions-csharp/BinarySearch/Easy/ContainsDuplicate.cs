8ed7b8245da21b9b036005169b1392fa3e011b2d
Weiss Jiang <meweissjiang@gmail.com>
2025-03-26 08:07:23 +1100

    added 217 Contains Duplicate

---
+++ leetcode-solutions-csharp/BinarySearch/Easy/ContainsDuplicate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_solutions_csharp.BinarySearch.Easy;
+
+public class ContainsDuplicate
+{
+    /*
+	* 217. Contains Duplicate
+	* Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+	*/
+
+    public void Run()
+    {
+        Console.WriteLine(Solution(new int[] { 1, 2, 3, 1 })); // true
+        Console.WriteLine(Solution(new int[] { 1, 2, 3, 4 })); // false
+        Console.WriteLine(Solution(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 })); // true
+        Console.WriteLine(Solution(new int[] { 4, 2, 1 })); // 1
+    }
+
+    public bool Solution(int[] nums)
+    {
+        return new HashSet<int>(nums).Count < nums.Length;
+    }
+}
