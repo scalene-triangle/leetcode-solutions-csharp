d7c907207fed294db28576d31500e1675fc83de2
Weiss Jiang <meweissjiang@gmail.com>
2025-05-01 07:21:02 +1000

    updated 121. Best Time to Buy and Sell Stock

--- leetcode-solutions-csharp/SlidingWindow/Easy/BestTimeToBuyAndSellStockI.cs
+++ leetcode-solutions-csharp/SlidingWindow/Easy/BestTimeToBuyAndSellStockI.cs
@@ -29,4 +29,37 @@ public class BestTimeToBuyAndSellStockI
 
         return maxProfit;
     }
+
+    // Aproach 2: Two Pointers
+    public int Solution2(int[] prices)
+    {
+        if (prices == null || prices.Length < 2)
+        {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        int leftBuy = 0;
+        int rightSell = 1;
+
+        while (rightSell < prices.Length)
+        {
+            int currentPrice = prices[rightSell];
+            int buyPrice = prices[leftBuy];
+
+            if (buyPrice < currentPrice)
+            {
+                int currentProfit = currentPrice - buyPrice;
+                maxProfit = Math.Max(maxProfit, currentProfit);
+            }
+            else
+            {
+                leftBuy = rightSell;
+            }
+
+            rightSell++;
+        }
+
+        return maxProfit;
+    }
 }
