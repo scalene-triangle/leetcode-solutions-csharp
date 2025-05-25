94f6348b638f3fcc5a246f8e98be6f16670191c5
Weiss Jiang <meweissjiang@gmail.com>
2025-05-25 10:38:15 +1000

    updated 3. Longest Substring Without Repeating Characters

--- leetcode-solutions-csharp/SlidingWindow/Medium/LongestSubstringWithoutRepeatingCharacters.cs
+++ leetcode-solutions-csharp/SlidingWindow/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -33,4 +33,29 @@ public class LongestSubstringWithoutRepeatingCharacters
 
         return maxCount;
     }
+
+    public int Solution2(string s)
+    {
+        List<char> letters = new List<char>();
+        int maxLength = 0;
+
+        foreach (char c in s)
+        {
+            if (!letters.Contains(c))
+            {
+                letters.Add(c);
+            }
+            else
+            {
+                maxLength = Math.Max(maxLength, letters.Count);
+
+                int idx = letters.IndexOf(c);
+                letters.RemoveRange(0, idx + 1);
+
+                letters.Add(c);
+            }
+        }
+
+        return Math.Max(maxLength, letters.Count);
+    }
 }
