db7d357f31df93513437a114a5d504cf6af63146
Weiss Jiang <meweissjiang@gmail.com>
2025-05-25 10:37:55 +1000

    added 3. Longest Substring Without Repeating Characters

---
+++ leetcode-solutions-csharp/SlidingWindow/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode_solutions_csharp.SlidingWindow.Medium;
+
+public class LongestSubstringWithoutRepeatingCharacters
+{
+    /*
+     * 3. Longest Substring Without Repeating Characters
+     * Given a string s, find the length of the longest substring without duplicate characters.
+     */
+    public void Run()
+    {
+        Console.WriteLine(Solution("abcabcbb")); // 3
+        Console.WriteLine(Solution("bbbbb")); // 1
+        Console.WriteLine(Solution("pwwkew")); // 3
+    }
+    public int Solution(string s)
+    {
+        var letters = new Dictionary<char, int>();
+        int maxCount = 0, left = 0, right;
+
+        for (right = 0; right < s?.Length; right++)
+        {
+            char letter = s[right];
+
+            if (letters.ContainsKey(letter))
+            {
+                left = Math.Max(left, letters[letter] + 1);
+            }
+
+            letters[letter] = right;
+
+            maxCount = Math.Max(maxCount, right - left + 1);
+        }
+
+        return maxCount;
+    }
+}
